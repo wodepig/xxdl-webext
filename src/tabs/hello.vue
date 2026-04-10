9da3dbcdfcf71ae9c73e948dad5afa34019fdc51
xxdl <xxdl@xxdl.top>
2026-04-10 10:04:16 +0800

    init

---
+++ src/tabs/hello.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    你好
+  </div>
+</template>
+
+<style>
+.container {
+  min-width: 470px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 47px;
+}
+.text-center {
+  text-align: center;
+}
+.action {
+  color: #470;
+  font-weight: bold;
+}
+</style>
+
+<script setup lang="ts">
+import { reactive } from "vue"
+import type { App } from "vue"
+
+const state = reactive({ count: 0, action: null })
+
+function increment() {
+  state.count++
+  state.action = "increment"
+}
+
+function decrement() {
+  state.count--
+  state.action = "decrement"
+}
+
+defineOptions({
+  prepare(app: App) {
+    // Use any plugins here:
+    // app.use
+  }
+})
+</script>
